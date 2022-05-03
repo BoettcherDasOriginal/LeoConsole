b1cf898249288f8cb4a7127369c508a3e2243544
BoettcherDasOriginal <boettcherleonard@gmail.com>
2022-05-03 18:32:07 +0200

    Update LeoConsole.cs

--- src/LeoConsole/LeoConsole.cs
+++ src/LeoConsole/LeoConsole.cs
@@ -310,7 +310,6 @@ namespace LeoConsole
 
             LConsole.Write($"§a{user.name}§r{afterUserName}>");
 
-            //Input = Console.ReadLine();
             Input = LConsole.ReadLine(commands);
 
             HandleInput();

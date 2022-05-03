c665f4c217c30e3ce5097cbecaaf82d7cfc195e3
BoettcherDasOriginal <boettcherleonard@gmail.com>
2022-05-03 16:59:02 +0200

    Update LConsole.cs

--- src/ILeoConsole/Core/LConsole.cs
+++ src/ILeoConsole/Core/LConsole.cs
@@ -21,93 +21,41 @@ namespace ILeoConsole.Core
                     case '§':
                         switch (chars[i + 1])
                         {
-                            case '0':
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                i++;
-                                break;
+                            case '0': Console.ForegroundColor = ConsoleColor.Black; i++; break;
 
-                            case '1':
-                                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                                i++;
-                                break;
+                            case '1': Console.ForegroundColor = ConsoleColor.DarkBlue; i++; break;
 
-                            case '2':
-                                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                                i++;
-                                break;
+                            case '2': Console.ForegroundColor = ConsoleColor.DarkGreen; i++; break;
 
-                            case '3':
-                                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                i++;
-                                break;
+                            case '3': Console.ForegroundColor = ConsoleColor.DarkCyan; i++; break;
 
-                            case '4':
-                                Console.ForegroundColor = ConsoleColor.DarkRed;
-                                i++;
-                                break;
+                            case '4': Console.ForegroundColor = ConsoleColor.DarkRed; i++; break;
 
-                            case '5':
-                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                i++;
-                                break;
+                            case '5': Console.ForegroundColor = ConsoleColor.DarkMagenta; i++; break;
 
-                            case '6':
-                                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                i++;
-                                break;
+                            case '6': Console.ForegroundColor = ConsoleColor.DarkYellow; i++; break;
 
-                            case '7':
-                                Console.ForegroundColor = ConsoleColor.Gray;
-                                i++;
-                                break;
+                            case '7': Console.ForegroundColor = ConsoleColor.Gray; i++; break;
 
-                            case '8':
-                                Console.ForegroundColor = ConsoleColor.DarkGray;
-                                i++;
-                                break;
+                            case '8': Console.ForegroundColor = ConsoleColor.DarkGray; i++; break;
 
-                            case '9':
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                                i++;
-                                break;
+                            case '9': Console.ForegroundColor = ConsoleColor.Blue; i++; break;
 
-                            case 'a':
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                i++;
-                                break;
+                            case 'a': Console.ForegroundColor = ConsoleColor.Green; i++; break;
 
-                            case 'b':
-                                Console.ForegroundColor = ConsoleColor.Cyan;
-                                i++;
-                                break;
+                            case 'b': Console.ForegroundColor = ConsoleColor.Cyan; i++; break;
 
-                            case 'c':
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                i++;
-                                break;
+                            case 'c': Console.ForegroundColor = ConsoleColor.Red; i++; break;
 
-                            case 'd':
-                                Console.ForegroundColor = ConsoleColor.Magenta;
-                                i++;
-                                break;
+                            case 'd': Console.ForegroundColor = ConsoleColor.Magenta; i++; break;
 
-                            case 'e':
-                                Console.ForegroundColor = ConsoleColor.Yellow;
-                                i++;
-                                break;
+                            case 'e': Console.ForegroundColor = ConsoleColor.Yellow; i++; break;
 
-                            case 'f':
-                                Console.ForegroundColor = ConsoleColor.White;
-                                i++;
-                                break;
+                            case 'f': Console.ForegroundColor = ConsoleColor.White; i++; break;
 
-                            case 'r':
-                                Console.ResetColor();
-                                i++;
-                                break;
+                            case 'r': Console.ResetColor(); i++; break;
 
-                            default:
-                                break;
+                            default: break;
                         }
                         break;
 
@@ -131,93 +79,41 @@ namespace ILeoConsole.Core
                     case '§':
                         switch (chars[i + 1])
                         {
-                            case '0':
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                i++;
-                                break;
+                            case '0': Console.ForegroundColor = ConsoleColor.Black; i++; break;
 
-                            case '1':
-                                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                                i++;
-                                break;
+                            case '1': Console.ForegroundColor = ConsoleColor.DarkBlue; i++; break;
 
-                            case '2':
-                                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                                i++;
-                                break;
+                            case '2': Console.ForegroundColor = ConsoleColor.DarkGreen; i++; break;
 
-                            case '3':
-                                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                i++;
-                                break;
+                            case '3': Console.ForegroundColor = ConsoleColor.DarkCyan; i++; break;
 
-                            case '4':
-                                Console.ForegroundColor = ConsoleColor.DarkRed;
-                                i++;
-                                break;
+                            case '4': Console.ForegroundColor = ConsoleColor.DarkRed; i++; break;
 
-                            case '5':
-                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                i++;
-                                break;
+                            case '5': Console.ForegroundColor = ConsoleColor.DarkMagenta; i++; break;
 
-                            case '6':
-                                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                i++;
-                                break;
+                            case '6': Console.ForegroundColor = ConsoleColor.DarkYellow; i++; break;
 
-                            case '7':
-                                Console.ForegroundColor = ConsoleColor.Gray;
-                                i++;
-                                break;
+                            case '7': Console.ForegroundColor = ConsoleColor.Gray; i++; break;
 
-                            case '8':
-                                Console.ForegroundColor = ConsoleColor.DarkGray;
-                                i++;
-                                break;
+                            case '8': Console.ForegroundColor = ConsoleColor.DarkGray; i++; break;
 
-                            case '9':
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                                i++;
-                                break;
+                            case '9': Console.ForegroundColor = ConsoleColor.Blue; i++; break;
 
-                            case 'a':
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                i++;
-                                break;
+                            case 'a': Console.ForegroundColor = ConsoleColor.Green; i++; break;
 
-                            case 'b':
-                                Console.ForegroundColor = ConsoleColor.Cyan;
-                                i++;
-                                break;
+                            case 'b': Console.ForegroundColor = ConsoleColor.Cyan; i++; break;
 
-                            case 'c':
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                i++;
-                                break;
+                            case 'c': Console.ForegroundColor = ConsoleColor.Red; i++; break;
 
-                            case 'd':
-                                Console.ForegroundColor = ConsoleColor.Magenta;
-                                i++;
-                                break;
+                            case 'd': Console.ForegroundColor = ConsoleColor.Magenta; i++; break;
 
-                            case 'e':
-                                Console.ForegroundColor = ConsoleColor.Yellow;
-                                i++;
-                                break;
+                            case 'e': Console.ForegroundColor = ConsoleColor.Yellow; i++; break;
 
-                            case 'f':
-                                Console.ForegroundColor = ConsoleColor.White;
-                                i++;
-                                break;
+                            case 'f': Console.ForegroundColor = ConsoleColor.White; i++; break;
 
-                            case 'r':
-                                Console.ResetColor();
-                                i++;
-                                break;
+                            case 'r': Console.ResetColor(); i++; break;
 
-                            default:
-                                break;
+                            default: break;
                         }
                         break;
 
